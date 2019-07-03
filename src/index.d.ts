307fd709a01d41499cbee246399432c1069cfa81
Satwik Gupta <satwik.cpp@gmail.com>
2019-07-03 16:25:28 +0530

    correct typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,22 +1,22 @@
-declare module 'OTPInput' {
-    import * as React from 'react';
+declare module "react-native-otp" {
+  import * as React from "react";
+  import * as RN from "react-native";
 
-    export interface OTPInputProps {
-      value?: string;
-      onChange?: Function;
-      otpLength?: number;
-      tintColor?: string;
-      offTintColor?: string;
-      containerStyle?: Object;
-      cellStyle?: Object;
-      defaultValue?: string;
-      editable?: boolean;
-      cellParentStyle?: Object;
-      showCellParentBorderColor: boolean;
-    }
+  export interface OTPInputProps extends RN.TextInputProps {
+    value?: string;
+    onChange?: Function;
+    otpLength?: number;
+    tintColor?: string;
+    offTintColor?: string;
+    containerStyle?: Object;
+    cellStyle?: Object;
+    defaultValue?: string;
+    editable?: boolean;
+    cellParentStyle?: Object;
+    showCellParentBorderColor: boolean;
+  }
 
-    export default class OTPInput extends React.Component<OTPInputProps, any> {
-        render(): JSX.Element;
-    }
+  export default class OTPInput extends React.Component<OTPInputProps, any> {
+    render(): JSX.Element;
+  }
 }
-
